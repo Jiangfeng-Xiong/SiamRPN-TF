e528d549997964f5db9a0b00cc2bafe569cec96c
jiangfeng-xiong <xiongjiangfeng@gmail.com>
2019-04-03 07:53:12 +0800

    update config.py

--- configs/config.py
+++ configs/config.py
@@ -4,11 +4,13 @@ root_dir="/home/lab-xiong.jiangfeng/Projects/SiameseRPN"
 
 RUN_NAME = "None"
 Model = "SiamRPN"
-batch_size=64
-base_lr = 0.1*(batch_size/64.0)
-embedding_checkpoint_file="/home/lab-xiong.jiangfeng/Projects/SiameseRPN/embeddings/pytorch_weights/siamrpn_model.pkl"
+batch_size=128
+base_lr = 0.01
+#embedding_checkpoint_file="/home/lab-xiong.jiangfeng/Projects/SiameseRPN/embeddings/pytorch_weights/siamrpn_model.pkl"
+embedding_checkpoint_file=None
 LOG_DIR = "Logs/%s"%(RUN_NAME)
 regloss_lambda = 10.0
+time_decay = True
 
 MODEL_CONFIG = {
   'z_image_size': 127,  # Exemplar image size
@@ -16,8 +18,9 @@ MODEL_CONFIG = {
   'field_size': 17,
   'net_shift': 63,
   'regloss_lambda': regloss_lambda,
+  'finetuned_checkpoint_file': osp.join(root_dir,"Logs/SiamRPN_fixedEmbedding/track_model_checkpoints/SiamRPN_fixedEmbedding"),
   'checkpoint': "%s/Logs/%s/track_model_checkpoints/%s"%(root_dir, RUN_NAME, RUN_NAME),
-  'embed_config': {'embedding_name': 'featureExtract_alexnet',
+  'embed_config': {'embedding_name': 'featureExtract_alexnet_fixedconv3',
                    'embedding_checkpoint_file': embedding_checkpoint_file,
                    'train_embedding': True,
                    'init_method': 'kaiming_normal',
@@ -33,20 +36,23 @@ MODEL_CONFIG = {
 TRAIN_CONFIG = {
   'train_dir': osp.join(LOG_DIR, 'track_model_checkpoints', RUN_NAME),
   'seed': 123,  # fix seed for reproducing experiments
-  'train_data_config': {'input_imdb': 'dataset/TrackingNet_VID_DET2014/train.pickle',
-						'lmdb_path': 'dataset/TrackingNet_VID_DET2014/train_lmdb_encode',
+  'train_data_config': {'input_imdb': 'dataset/DET2014_VID2015_LASOT_GOT10k/train.pickle',
+						'lmdb_path': 'dataset/DET2014_VID2015_LASOT_GOT10k/train_lmdb_encode',
 						'lmdb_encode': True,
-                        'num_examples_per_epoch': 1e6, #
-                        'epoch': 100,
+                        'time_decay':time_decay,
+                        'num_examples_per_epoch': 2e5, #
+                        'epoch': 200,
                         'batch_size':batch_size,
                         'max_frame_dist': 100,  # Maximum distance between any two random frames draw from videos.
                         'prefetch_threads': 16,
-                        'prefetch_capacity': 200 * batch_size,# The maximum elements number in the data loading queue
-                        'augmentation_config':{'random_flip': True, 'random_color': True,'random_blur': True}
+                        'prefetch_capacity': 50 * batch_size,# The maximum elements number in the data loading queue
+                        'augmentation_config':{'random_flip': True, 'random_color': True,'random_blur': True, 'random_downsample': False},
+                        'RandomMixUp': True
                         },  
-  'validation_data_config': {'input_imdb': 'dataset/TrackingNet_VID_DET2014/validation.pickle',
-							'lmdb_path': 'dataset/TrackingNet_VID_DET2014/validation_lmdb_encode',
+  'validation_data_config': {'input_imdb': 'dataset/GOT10k/validation.pickle',
+							'lmdb_path': 'dataset/GOT10k/validation_lmdb_encode',
 							'lmdb_encode': True,
+                            'time_decay': False,
                              'batch_size': 64,
                              'max_frame_dist': 100,  # Maximum distance between any two random frames draw from videos.
                              'prefetch_threads': 8,
@@ -61,7 +67,7 @@ TRAIN_CONFIG = {
   'lr_config': {'policy': 'exponential',
                 'initial_lr': base_lr,
                 'num_epochs_per_decay': 1,
-                'lr_decay_factor': 0.01**(1.0/100.0), #0.001^(1/(num_epoch-1))=0.8685 for sgd or 0.1^((1/num_epoch-1_)=0.954 for adam
+                'lr_decay_factor': 0.1**(1.0/100.0), #0.001^(1/(num_epoch-1))=0.8685 for sgd or 0.1^((1/num_epoch-1_)=0.954 for adam
                 'lr_warmup': False,
                 'staircase': True, },
 
@@ -72,7 +78,7 @@ TRAIN_CONFIG = {
   'log_every_n_steps': 10,
 
   # Frequency to save model
-  'save_model_every_n_step': 1e6 // batch_size// 2,  # save model every epoch
+  'save_model_every_n_step': 5000,  # save model every epoch
 
   # How many model checkpoints to keep. No limit if None.
   'max_checkpoints_to_keep': None,
@@ -87,6 +93,6 @@ TRACK_CONFIG = {
   'x_image_size': 255,  # Search image size during tracking
   'search_scale_smooth_factor': 0.3,
   'penalty_k': 0.22,
-  'window_influence': 0.4,
+  'window_influence': 0.2,
   'include_first': False, # If track the first frame
 }
